f28a7d62236c9f63a68aaa8dd6420dcdb9086095
Taylor Page <jonathantaylorpage@gmail.com>
2019-04-19 15:50:20 -0700

    (feat) Create on and off for play
    - Create pePlay function
    - Create play function
    - Create stop function
    - Allow creating of audio context on each play
    - Create debouncing for context creation
    
    Signed-off-by: Taylor Page <jontaylorpage@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,24 +5,9 @@ import Audio from './audio/Audio';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      webAudio: null
-    }
-  }
-
-  setWebAudioAPI( webAudio ) {
-    this.setState({
-      webAudio: webAudio,
-    }, () => {
-      // this.state.webAudio && this.state.webAudio.play();
-    });
-  }
   render() {
     return (
       <div className="App">
-        {/* <Upload setWebAudioAPI={ this.setWebAudioAPI.bind( this ) } ></Upload> */}
         <Audio></Audio>
         <Compressor></Compressor>
       </div>
--- src/audio/Audio.jsx
+++ src/audio/Audio.jsx
@@ -3,28 +3,56 @@ import React, { Component } from 'react';
 class Audio extends Component {
   constructor() {
     super();
+    this.playing = false;
+    this.state = {
+      played: false
+    };
     this.src = 'https://s3-us-west-1.amazonaws.com/orbitr-video/nebula_demo_loop.mp3';
   }
 
   componentDidMount() {
+    this.setupAudioContext();
+  }
+
+  setupAudioContext() {
     this.window = window;
     this.context = new ( this.window.AudioContext || this.window.webkitAudioContext )();
-    this.window.fetch( this.src )
+    return this.window.fetch( this.src )
       .then( res => res.arrayBuffer() )
       .then( buffer => this.context.decodeAudioData( buffer ) )
-      .then( buffer => this.buffer = buffer );
+      .then( buffer => this.buffer = buffer )
+      .then( () => {
+        this.source = this.context.createBufferSource();
+        this.source.buffer = this.buffer;
+        this.source.connect( this.context.destination );
+      });
   }
+
   play() {
-    const source = this.context.createBufferSource();
-    source.buffer = this.buffer;
-    source.connect( this.context.destination );
-    source.start();
+    this.setState({
+      played: true
+    }, () => this.source.start());
+  }
+
+  prePlay() {
+    !this.playing && ( this.state.played
+      ? this.setupAudioContext().then( () => this.play() )
+      : this.play() );
+    this.playing = true;
+  }
+
+  stop() {
+    this.playing = false;
+    this.setState({
+      playing: false,
+    }, () => this.source.stop());
   }
 
   render() {
     return(
       <div>
-        <button onClick={ this.play.bind( this ) }>play</button>
+        <button onClick={ this.prePlay.bind( this ) }>play</button>
+        <button onClick={ this.stop.bind( this ) }>pause</button>
       </div>
     );
   }
